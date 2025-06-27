e5061f99cdb5af23133e67be0d609488def88248
Naoyuki Tamura <tamura@kobe-u.ac.jp>
2025-06-27 11:18:14 +0900

    modified:   cspsat/examples/enigma_sim.py

--- cspsat/examples/enigma_sim.py
+++ cspsat/examples/enigma_sim.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+import getopt
+
 def chr2int(c):
     return ord(c) - ord("A")
 
@@ -124,3 +129,50 @@ class EnigmaSim:
             c1 = self.encipher(i+di, chr2int(c))
             cipher += int2chr(c1)
         return cipher
+
+def enigmaMain():
+    verbose = 0
+    rotors = [1,2,3]
+    rings = "AAA"
+    starts = "AAA"
+    plugConnections = []
+    opts, args = getopt.getopt(sys.argv[1:], "hvr:R:s:p:", ["help", "verbose=", "rotors=", "rings=", "starts=", "plugs="])
+    for o,x in opts:
+        if o in ["-h", "--help"]:
+            print("Usage: %s [options] plainText" % sys.argv[0])
+            print("  -h : help")
+            print("  -v, --verbose=N : verbose output")
+            print("  -r N1,N2,N2 --rotors=N1,N2,N3 : set rotors")
+            print("  -R XYZ --rings=XYZ : set ring positions")
+            print("  -s XYZ --starts=XYZ : set start positions")
+            print("  -p X1Y1,X2Y2,... --plugs=X1Y1,X2Y2,... : set plug connections")
+            print("Example: %s -v -r 2,1,3 -R SAT -s XYZ -p KO,BE,JP WEATHERXREPORT" % sys.argv[0])
+            return
+        elif o in ["-v"]:
+            verbose += 1
+        elif o in ["--verbose"]:
+            verbose = int(x)
+        elif o in ["-r", "--rotors"]:
+            rotors = [ int(r) for r in x.split(",") ]
+        elif o in ["-R", "--rings"]:
+            rings = x
+        elif o in ["-s", "--starts"]:
+            starts = x
+        elif o in ["-p", "--plugs"]:
+            plugConnections = x.split(",")
+    enigma = EnigmaSim(verbose=verbose)
+    enigma.setRotors(rotors)
+    enigma.setRingPositions(rings)
+    enigma.setStartPositions(starts)
+    enigma.setPlugboard(plugConnections)
+    plain = args[0]
+    print(f"Rotors: {rotors}")
+    print(f"Ring positions: {rings}")
+    print(f"Start positions: {starts}")
+    print(f"Plug connections: {plugConnections}")
+    print(f"Plain text: {plain}")
+    cipher = enigma.encipherStr(plain)
+    print(f"Cipher text: {cipher}")
+
+if __name__ == "__main__":
+    enigmaMain()
